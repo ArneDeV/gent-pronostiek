b1422aa6ba3d257341677a17dd70d4d494945414
Arne De Veylder <arne.de.veylder@proton.me>
2025-09-20 22:35:19 +0200

    fix erros

--- app/errors.py
+++ app/errors.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app, db
 
 @app.errorhandler(404)
@@ -7,4 +7,4 @@ def not_found_error(error):
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return redirect(url_for("matches"))
