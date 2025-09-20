6e8c7401248257392505691a8e4d3134248f4c87
Arne De Veylder <arne.de.veylder@proton.me>
2025-09-20 20:33:41 +0200

    Changes to init

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="/home/ArneDeV/gent-pronostiek/.env")
 
 app = Flask(__name__)
 app.config.from_object(Config)
