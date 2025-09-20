336bdfc3acf1079366f0e8c14749453e0978e8b8
Arne De Veylder <arne.de.veylder@proton.me>
2025-09-20 22:31:04 +0200

    fix error page

--- app/errors.py
+++ app/errors.py
@@ -1,10 +1,10 @@
-from flask import redirect, url_for
+from flask import render_template
 from app import app, db
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return redirect(url_for("matches"))
+    return render_template('500.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
-    return redirect(url_for("matches"))
+    return render_template('500.html'), 500
